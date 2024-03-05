2586e4e95879f37b4ca12f9eea8b76e2bbdf47a3
natdm02 <natalidimarino2002>
2024-03-05 16:26:03 +0100

    [add] html+ membri team

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// dettagli di ciascun membro del team.
+const team = [
+  {
+      name: "Wayne Barnett",
+      job: "Founder & CEO",
+      photo: "wayne-barnett-founder-ceo"
+  },
+  {
+      name: "Angela Caroll",
+      job: "Chief Editor",
+      photo: "angela-caroll-chief-editor.jpg"
+  },
+  {
+      name: "Walter Gordon",
+      job: "Office Manager",
+      photo: "walter-gordon-office-manager.jpg"
+  },
+  {
+      name: "Angela Lopez",
+      job: "Social Media Manager",
+      photo: "angela-lopez-social-media-manager.jpg"
+  },
+  {
+      name: "Scott Estrada",
+      job: "Developer",
+      photo: "scott-estrada-developer.jpg"
+  },
+  {
+      name: "Barbara Ramos",
+      job: "Graphic Designer",
+      photo: "barbara-ramos-graphic-designer.jpg"
+  }
+];
+
